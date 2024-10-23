7cbfa7deac0a90ed87121c23aa0ac5b0a83a9ef8
LXIV <themrsung@gmail.com>
2024-10-23 23:53:22 +0900

    Update cloakist-redirect.js

--- cloakist-redirect.js
+++ cloakist-redirect.js
@@ -5,6 +5,8 @@ const var2 = "https://www.themrsung.com" === url;
 const var3 = "https://themrsung.com/" === url;
 const var4 = "https://themrsung.com" === url;
 
-if (!(var1 || var2 || var3 || var4) && !window.location.href.includes("debugbypass")) {
+const debug = url.includes("debugbypass");
+
+if (!(var1 || var2 || var3 || var4 || debug)) {
   window.location.replace("https://www.themrsung.com/");
 }
